51ebb12e2b11fa25192cc62fea8c654736088216
369 <rob@jackbadger.co.uk>
2021-05-12 15:00:20 +0100

    changed to net framework 4.7.2 added system.text.json

---
+++ DocumentConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JBLib
+{
+    public class DocumentConfig
+    {
+        public List<string> Equations { get; set; }
+    }
+}
